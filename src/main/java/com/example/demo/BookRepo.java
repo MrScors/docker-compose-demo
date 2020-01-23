509dced96bd80c85d8ff047dc3ea64c13a1b59e4
Dmytro Pavlov <dpavlo@softserveinc.com>
2020-01-23 11:19:20 +0200

    Added more endpoints

--- src/main/java/com/example/demo/BookRepo.java
+++ src/main/java/com/example/demo/BookRepo.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface BookRepo extends CrudRepository<Book, Long> {
+    @Transactional
+    void deleteByAuthor(String s);
 }
--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.QueryParam;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +13,12 @@ import java.util.List;
 @RestController
 public class MainController {
 
-    @Autowired
+    final
     BookRepo bookRepo;
+
+    public MainController(BookRepo bookRepo) {
+        this.bookRepo = bookRepo;
+    }
 
     @GetMapping(path = "/hello")
     public String sayHello() {
@@ -25,10 +31,11 @@ public class MainController {
         for (Book b : bookRepo.findAll()) {
             books.add(b.toString());
         }
+
         return books;
     }
 
-    @GetMapping(path = "/create")
+    @PostMapping(path = "/create")
     public String createBook(@QueryParam("author") String author,
                              @QueryParam("title") String title) {
         Book book = new Book(author, title);
@@ -37,7 +44,46 @@ public class MainController {
         } catch (Exception e) {
             return "Error";
         }
+
         return "Book created";
+    }
+
+
+    @PutMapping(path = "/update")
+    public String updateBook(@RequestParam("id") Long id,
+                             @RequestParam(defaultValue = "none") String author,
+                             @RequestParam(defaultValue = "none") String title){
+        try {
+
+            Book book = bookRepo.findById(id).orElse(null);
+            assert book != null;
+
+            if (!author.intern().equals("none")) {
+                book.setAuthor(author);
+            }
+            if (!title.intern().equals("none")) {
+                book.setTitle(title);
+            }
+            bookRepo.save(book);
+
+        } catch (AssertionError e) {
+            return "Failed to find book";
+        } catch (Exception e) {
+            return e.toString();
+        }
+
+        return "Updated";
+    }
+
+    @DeleteMapping(path = "/deleteByAuthor")
+    public String deleteBookByAuthor(@QueryParam("author") String author) {
+        try {
+            bookRepo.deleteByAuthor(author);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+
+        return "Deleted";
     }
 
 }
